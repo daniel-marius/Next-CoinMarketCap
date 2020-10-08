8cec35db631893c2554519b40751b9307b1ab7b5
danielmarius1995 <50268474+danielmarius1995@users.noreply.github.com>
2020-10-09 00:00:10 +0300

    First commit

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+## Features
+
+- Fetching data from [https://coinmarketcap.com/](https://coinmarketcap.com/) using [CoinGecko API](https://www.coingecko.com/en/api)
+
 ## Getting Started
 
 First, run the development server:
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,68 @@
-import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
+import CoinGecko from 'coingecko-api';
 
-export default function Home() {
+const coinGeckoClient = new CoinGecko();
+
+export default function Home(props) {
+  const { data } = props.result;
+
+  const formatPercent = (number) => {
+    return `${new Number(number).toFixed(2)}%`;
+  };
+
+  const formDollar = (number, maxSignificantDigits) => {
+    return new Intl.NumberFormat(
+      'en-US',
+      {
+        style: 'currency',
+        currency: 'usd',
+        maxSignificantDigits
+      }
+    ).format(number);
+  };
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>CoinMarketCap</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1>CoinMarketCap</h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>24H Change</th>
+            <th>Price</th>
+            <th>Market Cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(coin => (
+            <tr key={coin.id}>
+              <td>
+                <img
+                  src={coin.image}
+                  style={{width: 25, height: 25, marginRight: 10}}
+                />
+                {coin.symbol.toUpperCase()}
+              </td>
+              <td>
+                <span className={coin.price_change_percentage_24h > 0 ? (
+                  'text-success' ) : 'text-danger'}
+                >
+                  {formatPercent(coin.price_change_percentage_24h)}
+                </span>
+              </td>
+              <td>{formDollar(coin.current_price, 20)}</td>
+              <td>{formDollar(coin.market_cap, 12)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
       <style jsx>{`
         .container {
@@ -206,4 +211,16 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const params = {
+    order: CoinGecko.ORDER.MARKET_CAP_DESC
+  };
+  const result = await coinGeckoClient.coins.markets({params});
+  return {
+    props: {
+      result
+    },
+  }
 }
